933e9d3fdfe3c88d63f1f3c11f17c8e0ae27855e
Matt Yu <lwlmm_77@163.com>
2021-02-26 04:00:01 +0800

    Add BFS to BinaryTree

--- Data/BinaryTree/main.go
+++ Data/BinaryTree/main.go
@@ -81,6 +81,40 @@ func dfsDivideResult(root *TreeNode) []int {
 	return result
 }
 
+//BFS广度遍历
+func levelOrder(root *TreeNode) [][]int {
+    // 通过上一层的长度确定下一层的元素
+    result := make([][]int, 0)
+    if root == nil {
+        return result
+    }
+    queue := make([]*TreeNode, 0)
+    queue = append(queue, root)		//len(queue) = 1
+    for len(queue) > 0 {
+        list := make([]int, 0)
+        // 为什么要取length？
+        // 记录当前层有多少元素（遍历当前层，再添加下一层）
+        l := len(queue)
+        for i := 0; i < l; i++ {
+
+            // 出队列
+            level := queue[0]
+            queue = queue[1:]
+            list = append(list, level.Val)
+
+			//进队列
+            if level.Left != nil {
+                queue = append(queue, level.Left)
+            }
+            if level.Right != nil {
+                queue = append(queue, level.Right)
+            }
+        }
+        result = append(result, list)
+    }
+    return result
+}
+
 
 func main() {
 
@@ -126,4 +160,10 @@ func main() {
 	//fmt.Println(dfsResult(root))
 	fmt.Println(dfsResult(right1))
 	fmt.Println(dfsDivideResult(root))
+
+	fmt.Println(levelOrder(root))
+
+	queue := make([]*TreeNode, 0)
+    queue = append(queue, root)
+	fmt.Println(len(queue))
 }
